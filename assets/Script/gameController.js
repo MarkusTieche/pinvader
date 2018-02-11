70d90740411890ade4a863508e2fe01a09fe9630
markusT <info@inkfood.com>
2018-02-11 14:33:21 +0100

    first commit

---
+++ assets/Script/gameController.js
@@ -0,0 +1,44 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        flipperLeft:cc.Node,
+        flipperRight:cc.Node,
+        ball:cc.Node
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        
+        cc.view.enableAntiAlias(false);
+        
+        var physicsManager = cc.director.getPhysicsManager();
+            physicsManager.enabled = true;
+        
+//        physicsManager.debugDrawFlags = 
+//        // 0;
+//        // cc.PhysicsManager.DrawBits.e_aabbBit |
+//        // cc.PhysicsManager.DrawBits.e_pairBit |
+//        // cc.PhysicsManager.DrawBits.e_centerOfMassBit |
+//        cc.PhysicsManager.DrawBits.e_jointBit |
+//        cc.PhysicsManager.DrawBits.e_shapeBit;
+//        
+//        cc.director.getCollisionManager().enabledDebugDraw = false;
+        
+        
+        this.flipperLeft.rotation = 20;
+        this.flipperRight.rotation = -20;
+        
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, function(){  this.flipperLeft.rotation = -20;this.flipperRight.rotation = 20;}, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, function(){  this.flipperLeft.rotation = 20;this.flipperRight.rotation = -20;}, this);
+    },
+    
+    update:function()
+    {
+      if(this.ball.getPositionY()<-250)
+      {
+          //RESET BALL POSITION
+         this.ball.setPosition(-40,0)
+      }  
+    },
+});
